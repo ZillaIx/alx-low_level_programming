82b1433f904a34d9ccf6b37cd6e6d4a5067d0b9f
Bethel Joseph <zillaraytor@gmail.com>
2023-02-17 12:51:19 -0800

    More Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Main Function
+ *
+ * Return: always 0
+ */
+
+int main(void)
+
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	putchar('\n');
+	return (0);
+}
