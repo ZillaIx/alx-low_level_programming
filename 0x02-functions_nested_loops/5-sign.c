97627760ec5a883228e0a2f10e313f9521c0b229
Bethel Joseph <zillaraytor@gmail.com>
2023-02-21 12:04:13 -0800

    Signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_sign - function that prints signs
+ *
+ * @n: parameter for function
+ *
+ *Return: 1, 0 amd -1
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		return (-1);
+	}
+}
