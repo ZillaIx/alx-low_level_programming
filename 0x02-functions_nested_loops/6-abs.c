8a0327b8753156104b4adc62db2c076475b0a392
Bethel Joseph <zillaraytor@gmail.com>
2023-02-21 12:55:54 -0800

    Abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - checks absolute values
+ *
+ * @a: parameter to be checked
+ *
+ * Return: always a
+ */
+
+int _abs(int a)
+{
+	if (a < 0)
+		a = -(a);
+	else if (a >= 0)
+		a = a;
+	return (a);
+}
